cbd45c96e8868f4838973f41a333e7a0777ea92a
Lee Wangenheim <lee@ChrUbuntu.(none)>
2013-05-19 17:56:34 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -115,4 +115,9 @@ describe "with a password that's too short" do
       specify { user_for_invalid_password.should be_false }
     end
   end
+    
+  describe "remember token" do
+     before { @user.save }
+     its(:remember_token) { should_not be_blank }
+  end
 end
