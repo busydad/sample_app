869913e62e77982c550c04aea31f218a9eb846b0
Lee Wangenheim <lee@ChrUbuntu.(none)>
2013-05-20 21:24:04 -0400

    Add user microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130520152607) do
+ActiveRecord::Schema.define(:version => 20130520235718) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
